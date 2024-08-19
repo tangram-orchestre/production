748ff7e5a70fc948436c3d1f5ab283830b197a2e
Luc Varoqui <luc@varoqui.org>
2024-08-19 22:23:40 +0000

    switch to auto deployed showcase-website

---
+++ update-showcase-website.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+docker compose pull showcase-website
+docker compose up -d showcase-website
